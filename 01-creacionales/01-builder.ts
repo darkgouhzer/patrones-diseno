e288f9979df3df68706c560d82de8b4d7da48641
Hugo Bojorquez <darkgouhzer@gmail.com>
2026-01-12 21:46:38 -0700

    Add creational design patterns and helper functions

---
+++ 01-creacionales/01-builder.ts
@@ -0,0 +1,96 @@
+/**
+ * ! Patrón Builder:
+ * Es un patrón de diseño creacional que nos permite construir objetos complejos
+ * paso a paso.
+ *
+ * El patrón nos permite producir distintos tipos y representaciones
+ * de un objeto empleando el mismo código de construcción.
+ *
+ * * Es útil cuando necesitamos construir un objeto complejo con muchas partes
+ * * y queremos que el proceso de construcción sea independiente de las partes
+ * * que lo componen.
+ *
+ * https://refactoring.guru/es/design-patterns/builder
+ */
+
+import { COLORS } from "../helpers/colors.ts";  
+class Computer{
+    public cpu: string = 'cpu - not defined';
+    public ram: string = 'ram - not defined';
+    public storage: string = 'storage - not defined';
+    public gpu?: string;
+
+    displayConfiguration(){
+        console.log(`Configuración de la computadora
+            CPU: ${ this.cpu }
+            RAM: ${ this.ram }
+            Almacenamiento: ${ this.storage }
+            GPU: ${ this.gpu }            
+            `);
+    }
+}
+
+
+class ComputerBuilder{
+
+    private computer:Computer;
+
+    constructor(){
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string):ComputerBuilder{
+
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setRAM(ram: string): ComputerBuilder{
+    
+        this.computer.ram = ram;
+        return this;
+    }
+
+    setStorage(storage: string): ComputerBuilder{
+    
+        this.computer.storage = storage;
+        return this;
+    }
+
+    setGPU(gpu: string): ComputerBuilder{
+    
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build(){
+        return this.computer;
+    }
+
+}
+
+
+
+function main(){
+    const basicComputer = new ComputerBuilder()
+        .setCPU("Intel Core 2 Duo")
+        .setRAM("4GB")
+        .setStorage("256GB")
+        .build();
+        console.log("%cComputadora básica:", COLORS.blue);
+
+        basicComputer.displayConfiguration();
+
+    const gamerComputer = new ComputerBuilder()
+        .setCPU("Intel i9 14th")
+        .setRAM("32GB")
+        .setStorage("8TB WD BLACK 850 nvme 2280")
+        .setGPU("Nvidia RTX 5090 ti")
+        .build();
+
+        console.log("%cComputadora Gamer:", COLORS.blue);
+
+        gamerComputer.displayConfiguration();
+}
+
+main();
